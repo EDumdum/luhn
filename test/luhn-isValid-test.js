799fccb8512e242baa62fc3cd2fc9596b14f574a
edum <etienne.g.dumont@gmail.com>
2017-10-24 14:00:56 +0200

    Correct test isValid

--- test/luhn-isValid-test.js
+++ test/luhn-isValid-test.js
@@ -9,16 +9,16 @@ const expect = chai.expect;
 const luhn = require('../src/luhn');
 
 describe('luhn-isValid', function() {
-    it('Check inelligible input (null, undefined, not a string or number)', function() {
+    it('Check inelligible input (null, undefined, not a string)', function() {
         expect(function() { 
             luhn.isValid(null); 
-        }).to.throw('Expecting value of type \'string\' or \'number\', found: \'null\'');
+        }).to.throw('Expecting value of type \'string\', found: \'null\'');
         expect(function() { 
             luhn.isValid(undefined); 
-        }).to.throw('Expecting value of type \'string\' or \'number\', found: \'undefined\'');
+        }).to.throw('Expecting value of type \'string\', found: \'undefined\'');
         expect(function() { 
             luhn.isValid([]); 
-        }).to.throw('Expecting value of type \'string\' or \'number\', found: \'object\'');
+        }).to.throw('Expecting value of type \'string\', found: \'object\'');
     });
 
     it('Check input value format, excepted /^[0-9]{2,}$/', function() {
@@ -34,19 +34,6 @@ describe('luhn-isValid', function() {
         expect(function() {
             luhn.isValid('123a');
         }).to.throw('Exception value of format \'/^[0-9]{2,}$/\', found: \'123a\'');
-
-        expect(function() {
-            luhn.isValid(0);
-        }).to.throw('Exception value of format \'/^[0-9]{2,}$/\', found: \'0\'');
-        expect(function() {
-            luhn.isValid(1);
-        }).to.throw('Exception value of format \'/^[0-9]{2,}$/\', found: \'1\'');
-        expect(function() {
-            luhn.isValid(-1);
-        }).to.throw('Exception value of format \'/^[0-9]{2,}$/\', found: \'-1\'');
-        expect(function() {
-            luhn.isValid(1.3);
-        }).to.throw('Exception value of format \'/^[0-9]{2,}$/\', found: \'1.3\'');
     });
 
     it('Check return with elligible value', function() {
@@ -54,10 +41,5 @@ describe('luhn-isValid', function() {
         expect(luhn.isValid('987654321')).eq(false);
         expect(luhn.isValid('5785')).eq(true);
         expect(luhn.isValid('987654324')).eq(true);
-
-        expect(luhn.isValid(1450)).eq(false);
-        expect(luhn.isValid(123456789)).eq(false);
-        expect(luhn.isValid(18)).eq(true);
-        expect(luhn.isValid(123456782)).eq(true);
     });
 });
